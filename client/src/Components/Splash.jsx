27cb2bd82d5a78356ceaf5986f2222df1612c34c
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-11-20 11:03:38 -0500

    styled up splash page

--- client/src/Components/Splash.jsx
+++ client/src/Components/Splash.jsx
@@ -12,10 +12,19 @@ class Splash extends Component {
     return (
       <div className="splash-body">
         <main className="splash-main">
-          <h1 className="splash-header">
-            Welcome to the coolest app in the world
-          </h1>
-          <p className="splash-welcome">Have a blast here, friend!</p>
+          <h1 className="splash-header">Welcome to Find a Bro.</h1>
+          <img
+            className="splash-logo"
+            src="./images/mustard-wolf.png"
+            alt="wolf logo"
+          />
+          <h2 className="splash-informative">
+            A place for men to make new friends.
+          </h2>
+          <p className="splash-welcome">
+            Just log in, answer a few questions about yourself, and find a new
+            bro!
+          </p>
           <div>
             <button onClick={this._login} className="login">
               Log In
